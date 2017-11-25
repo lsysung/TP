2facd214eeb3b69d76d2af89f82b9b73617511b4
sr092 <sr0925@daum.net>
2017-11-25 17:58:12 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tp/SuccessActivity.java
@@ -0,0 +1,17 @@
+package com.example.tp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sr092 on 2017-11-18.
+ */
+
+public class SuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+    }
+}
